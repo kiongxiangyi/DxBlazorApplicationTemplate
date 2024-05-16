d5f73de841c78b43f81db07664853deaefb84eaa
kiong <kiong@H0DKS04>
2024-05-16 13:35:29 +0200

    Add project files.

---
+++ DxBlazorApplication1/DxBlazorApplication1/Program.cs
@@ -0,0 +1,39 @@
+using DxBlazorApplication1.Components;
+using DxBlazorApplication1.Data;
+using DxBlazorApplication1.Client.Pages;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddDevExpressBlazor(options => {
+    options.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
+    options.SizeMode = DevExpress.Blazor.SizeMode.Medium;
+});
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddMvc();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if(app.Environment.IsDevelopment()) {
+    app.UseWebAssemblyDebugging();
+} else {
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+app.Run();
